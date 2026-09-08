a8ea43679e17d4835cc777433f5261c614b8d399
shymarkky <gayramamarklawrence@gmail.com>
2026-09-08 10:23:18 +0800

    added migration

--- lab3.sql
+++ lab3.sql
@@ -46,3 +46,34 @@ JOIN books b on
 
 WHERE br.borrow_return_date is NULL
 ORDER BY br.borrow_date DESC;
+
+
+UPDATE borrow
+SET borrow_return_date = CURRENT_TIMESTAMP
+-- Set borrow id fro the borrowed book you want to return
+WHERE borrow_id = 4 AND borrow_return_date IS NULL;
+
+SELECT
+br.borrow_id,
+
+CONCAT(
+ s.student_first_name,
+' ',
+ s.student_last_name
+) as student_name,
+
+
+b.book_title,
+b.book_author,
+b.book_category,
+
+br.borrow_date,
+br.borrow_return_date
+FROM borrow br
+JOIN students s ON
+    br.student_id = s.student_id
+JOIN books b ON
+    br.book_id = b.book_id
+
+WHERE br.borrow_return_date is NOT NULL
+ORDER BY br.borrow_date DESC;
